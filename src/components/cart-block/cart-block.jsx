1dcf610db89969320cc9470abec5e46c5efc5e66
Rustdj <81041717+Rustdj@users.noreply.github.com>
2022-05-05 10:34:00 +0300

    adding to cart

--- src/components/cart-block/cart-block.jsx
+++ src/components/cart-block/cart-block.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import './cart-block.css';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import React, { useState } from "react";
+import "./cart-block.css";
+import { useSelector } from "react-redux";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { calcTotalPrice } from "../utils";
+import { CartMenu } from "../cart-menu/cart-menu";
+import { ItemsInCart } from "../items-in-cart/items-in-cart";
 
 export const CartBlock = () => {
+  const [isCartMenuVisible, setIsCartMenuVisible] = useState(false);
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = calcTotalPrice(items);
   return (
     <div className="cart-block">
-        <ShoppingCartIcon className="cart-block__icon"/>
-        <span className="cart-block__total-price">1890 руб.</span>
+      <ItemsInCart quantity={items.length}/>
+      <ShoppingCartIcon
+        className="cart-block__icon"
+        onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}
+      />
+      {totalPrice > 0 ? (
+        <span className="cart-block__total-price">{totalPrice} руб.</span>
+      ) : null}
+      {isCartMenuVisible && <CartMenu items={items} onClick={() => null} />}
     </div>
-  )
-}
+  );
+};
