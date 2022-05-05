47097d358e53b9ab3c0fb8423fe73a2443ad5cca
Rustdj <81041717+Rustdj@users.noreply.github.com>
2022-05-05 10:34:00 +0300

    adding to cart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { HomePage } from './pages/home-page/home-page.jsx';
 import { Header } from './components/header/header';
+import { store } from './redux';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-        </Routes>
-    </div>
+    
+      <div className="App">
+        <Provider store={store}>
+          <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </Provider>
+      </div>
+    
   );
 }
 
